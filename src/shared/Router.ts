546a3af1ca08282a8fc8e061feec28ef39c2b846
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-07-14 14:08:33 -0400

    Added Eventer, some helper types, some JSDoc comments, and a region for setup in Router.

--- src/shared/Router.ts
+++ src/shared/Router.ts
@@ -1,13 +1,79 @@
+//#region Setup
+import { SuccessCase, ValueSuccessCase } from "./Classes";
+
 const ReplicatedStorage = game.GetService("ReplicatedStorage");
 const RunService = game.GetService("RunService");
-
-type Callback<T extends unknown[] = unknown[], R = void> = (...args: T) => R;
 
 const isServer = RunService.IsServer();
 const isClient = RunService.IsClient();
 
-class Router {
+let RouterFolder: Folder;
+let RemoteFunctions: Folder;
+if (isClient) {
+	RouterFolder = ReplicatedStorage.WaitForChild("Router") as Folder;
+} else if (isServer) {
+	let tRouterFolder = ReplicatedStorage.FindFirstChild("Router") as Folder | undefined;
+	if (tRouterFolder === undefined) {
+		tRouterFolder = new Instance("Folder", ReplicatedStorage);
+		RemoteFunctions = new Instance("Folder", tRouterFolder);
+	}
+	RouterFolder = tRouterFolder;
+}
+//#endregion
 
+/**
+ * Routes messages between scripts.
+ */
+class Router {
+	static RequestFromServer(type: string, ...args: unknown[]): ValueSuccessCase<unknown[] | undefined> {
+		const event: RemoteFunction | undefined = RemoteFunctions.FindFirstChild(type) as RemoteFunction | undefined;
+		if (event === undefined) {
+			return ValueSuccessCase.Fail(`No C->S->C of type ${type} was found`, undefined);
+		}
+		return ValueSuccessCase.Ok("Got data from server.", event.InvokeServer(...args));
+	}
+}
+
+/**
+ * Represents a function to unlink an event.
+ */
+type Unlinker<> = () => SuccessCase;
+
+/**
+ * Represents a callback for an event.
+ */
+type EventCallback<I extends unknown[]> = (...args: I) => undefined;
+
+class Eventer<I extends unknown[], O extends unknown[]> {
+	private readonly event: RemoteEvent;
+
+	Fire(...args: I) {
+
+	}
+
+	OnClient(callback: EventCallback<I>) {
+		if (!isClient) {
+			error("Can only link to client as the client", 2);
+		}
+		this.event.OnClientEvent.Connect(callback);
+	}
+
+	constructor(token: string) {
+		let tEvent: RemoteEvent;
+
+		if (isServer) {
+			const xEvent = ReplicatedStorage.FindFirstChild(token) as RemoteEvent | undefined;
+			if (xEvent === undefined) {
+				tEvent = new Instance("RemoteEvent", ReplicatedStorage);
+				tEvent.Name = token;
+			} else {
+				tEvent = xEvent;
+			}
+		} else {
+			tEvent = ReplicatedStorage.WaitForChild(token) as RemoteEvent;
+		}
+		this.event = tEvent;
+	}
 }
 
 export default Router;
