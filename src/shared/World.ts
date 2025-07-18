72232ff3664b6a2d1ae65e953f5785a71a2d0599
MicroGamerSM <paul.l.strobeck@gmail.com>
2025-07-18 17:35:42 -0400

    Made Item readonly, added Recipe.

--- src/shared/World.ts
+++ src/shared/World.ts
@@ -1,16 +1,29 @@
 import JSONItem from "./json/item";
+import JSONRecipe from "./json/recipe";
 
 export class Item {
-	id: string;
-	name: string;
-	description: string;
-	tags: string[];
+	readonly id: string;
+	readonly name: string;
+	readonly description: string;
+	readonly tags: string[];
 
 	constructor(core: JSONItem) {
 		this.id = core.id;
 		this.name = core.name;
 		this.description = core.description;
 		this.tags = core.tags;
+	}
+}
+
+export class Recipe {
+	readonly type: string;
+	readonly inputs: Map<string, number>;
+	readonly outputs: Map<string, number>;
+
+	constructor(core: JSONRecipe) {
+		this.type = core.type;
+		this.inputs = core.inputs;
+		this.outputs = core.outputs;
 	}
 }
 
